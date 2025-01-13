90e88f7fd6bbb4f02b3e0f4b1ba6028adf474779
= <=>
2025-01-14 01:28:40 +0530

    Changed navbar css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,26 @@
+import { useEffect } from 'react'
 import styles from '../css/navbar.module.css'
 
 export const Navbar = () => {
+  useEffect(()=>{
+    const listener = (e)=>{
+      const navbar = document.getElementById("navbar");
+      if(window.scrollY >= 90){
+        navbar.classList.add(styles.onScroll);
+      }
+      else{
+        navbar.classList.remove(styles.onScroll);
+      }
+    }
+
+    window.addEventListener('scroll',listener)
+    return () =>{
+      window.removeEventListener('scroll',listener)
+    }
+
+  },[])
   return (
-    <div className={styles.container}>
+    <div id="navbar" className={`${styles.container}`}>
         <div className={styles.logoWrapper}>
             <img src="/logo1.png" alt="logo1" className={styles.logo1}/>
             <img src="logo2.png" alt="logo2" className={styles.logo2}/>
